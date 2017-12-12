34a752a0149f9a7113c44071735c2876ccce3e2f
Piero Giusti <pierophp@gmail.com>
2017-12-12 11:26:58 -0200

    Nginx redirect

--- dnsmasq/entrypoint.sh
+++ dnsmasq/entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+
 rm /etc/dnsmasq.conf
 cp /etc/dnsmasq.conf.template /etc/dnsmasq.conf
+
 CONSUL_HOST=`getent hosts consul | awk '{ print $1 }'`
-getent hosts consul | awk '{ print $1 }' | sed -i "s/CONSUL_HOST/$CONSUL_HOST/" /etc/dnsmasq.conf
+sed -i "s/CONSUL_HOST/$CONSUL_HOST/" /etc/dnsmasq.conf
+
+NGINX_HOST=`getent hosts consul_nginx | awk '{ print $1 }'`
+sed -i "s/NGINX_HOST/$NGINX_HOST/" /etc/dnsmasq.conf
 
 webproc --config /etc/dnsmasq.conf -- dnsmasq --no-daemon
