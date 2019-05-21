73b4bf9c42b646d21c5d5b31fbfcf1e4d1f81337
Piero Giusti <pierophp@gmail.com>
2019-05-21 11:55:43 -0300

    Https Development

---
+++ nginx/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+which openssl || apt update 
+which openssl || apt install -y openssl
+
+test -f /etc/ssl/private/_develop.key || echo "Generating Certificate .develop"
+test -f /etc/ssl/private/_develop.key || openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/_develop.key -out /etc/ssl/certs/_develop.crt -subj "/C=BR/ST=Sao Paulo/L=Sao Paulo/O=Local Organization/OU=IT Department/CN=*.develop"
+
+test -f /etc/ssl/private/_local.bbrands.key || echo "Generating Certificate .develop"
+test -f /etc/ssl/private/_local.bbrands.key || openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/_local.bbrands.key -out /etc/ssl/certs/_local.bbrands.crt -subj "/C=BR/ST=Sao Paulo/L=Sao Paulo/O=Local Organization/OU=IT Department/CN=*.local.bbrands.com.br"
+
+
+echo "Starting Nginx"
+nginx -g "daemon off;"   
