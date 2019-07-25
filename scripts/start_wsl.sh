24679d39aa5a2f19895cae6c6ce4d83e43f7e2a4
Piero Giusti <pierophp@gmail.com>
2019-07-25 16:15:50 -0300

    WSL

---
+++ scripts/start_wsl.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+if [ ! "$EUID" -ne 0 ]
+  then echo "Please don't run as root"
+  exit
+fi
+
+BASEDIR=$(dirname "$0")
+cd $BASEDIR/../
+
+# Create Network Consul
+[[ $(docker network ls | grep consul) ]] || docker network create consul
+
+docker-compose  -f docker-compose.yml -f docker-compose-wsl.yml up -d
+
+DNSMASQ_IP=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' consul_dnsmasq`
+
+echo "
+nameserver ${DNSMASQ_IP}
+nameserver 127.0.0.1
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+" > ./resolv.conf
+
+sudo mv -f ./resolv.conf /etc/resolv.conf
--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 UNAMESTR=`uname`
-if [ $UNAMESTR == 'Darwin' ]; then
+
+if [ -f "/mnt/c/WINDOWS/explorer.exe" ]; then
+    ./scripts/start_wsl.sh
+elif [ $UNAMESTR == 'Darwin' ]; then
     ./scripts/start_osx.sh
 else 
     ./scripts/start_linux.sh
